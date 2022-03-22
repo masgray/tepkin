779bfdb2bc1207b277cc18993cafe6f46aa31dd4
masgray <31476419+masgray@users.noreply.github.com>
2022-03-22 15:30:42 +0300

    Feedback fixes.

--- trainer_v2/index.js
+++ trainer_v2/index.js
@@ -1,39 +1,33 @@
 const testsTitles = config.testsTitles;
 const answers = config.answers;
 const trueAnswers = config.trueAnswers;
-const podskazka = config.podskazka;
+const toLearn = config.toLearn;
 const testsCount = testsTitles.length;
 const answersCount = answers[0].length;
 
 const results = testsTitles.map((_, idx) => ({ testIdx: idx, correct: false }));
 let testIndex = -1;
-let podskazkaVisible = false;
-let clickedButton = -1;
 
-const zagalovok = document.getElementById("zadacha");
+const title = document.getElementById("zadacha");
 const answerButtons = [];
 const buttonNext = document.getElementById('btn10');
-const text2 = document.getElementById("otvet");
+const answerResult = document.getElementById("otvet");
+const taskDiv = document.getElementById("task");
+const resultDiv = document.getElementById("result");
 
 function nextTest() {
-    podskazkaVisible = false;
     buttonNext.disabled = true;
     testIndex++;
     if (testIndex >= testsCount) {
-        let s1 = results.some((v) => v.correct)
-            ? "<br>Успешные задания: " + results.filter((v) => v.correct).map((v) => testsTitles[v.testIdx]).join(", ")
-            : "";
-        let s2 = results.some((v) => !v.correct)
-            ? "<br>Проваленные задания: " + results.filter((v) => !v.correct).map((v) => testsTitles[v.testIdx]).join(", ")
-            : "";
-        text2.innerHTML = "Задания выполнены! " + s1 + s2;
-        for (let i = 0; i < answersCount; ++i) {
-            answerButtons[i].style.display = "none";
-        }
+        taskDiv.style.display = "none";
+        resultDiv.innerHTML = "<p>Задания выполнены!</p>" +
+            (results.some((v) => !v.correct)
+            ? "Повтори следующий материал:<ul><li>" + results.filter((v) => !v.correct)
+                .map((v) => toLearn[v.testIdx]).join("<li>") + "</ul>"
+            : "");
     } else {
-        clickedButton = -1;
-        zagalovok.textContent = testsTitles[testIndex];
-        text2.textContent = "";
+        title.textContent = testsTitles[testIndex];
+        answerResult.textContent = "";
         for (let i = 0; i < answersCount; ++i) {
             answerButtons[i].textContent = answers[testIndex][i];
             answerButtons[i].style.backgroundColor = '#D1D1D1';
@@ -48,25 +42,14 @@ for (let i = 0; i < answersCount; ++i) {
     btn.addEventListener('click', () => {
         if (i === trueAnswers[testIndex]) {
             answerButtons[i].style.backgroundColor = '#0F0';
-            text2.textContent = "Верно!";
+            answerResult.textContent = "Верно!";
             results[testIndex].correct = true;
-            answerButtons.forEach((b) => b.disabled = true);
-            buttonNext.disabled = false;
         } else {
             answerButtons[i].style.backgroundColor = '#F00';
-            if (podskazkaVisible) {
-                if (i == clickedButton) {
-                    return;
-                }
-                text2.textContent = "Не верно!";
-                answerButtons.forEach((b) => b.disabled = true);
-                buttonNext.disabled = false;
-            } else {
-                text2.textContent = podskazka[testIndex];
-                podskazkaVisible = true;
-                clickedButton = i;
-            }
+            answerResult.innerHTML = "Не верно!<br>Повтори следующий материал: " + toLearn[testIndex];
         }
+        answerButtons.forEach((b) => b.disabled = true);
+        buttonNext.disabled = false;
     });
 }
 
